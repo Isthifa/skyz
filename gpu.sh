428af1dc69a4e20ca842dfa86f55bd55ac2565a8
Isthifa <85566406+Isthifa@users.noreply.github.com>
2021-06-25 16:23:42 +0530

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=TRX:TEpXXj9hc2eJ6YBqBY1WbY3GzEp3C2TmV6
+WALLET=WIN:TNm3k3uCQXtfYhwPZFzNq4dTCPumBSfYNp.net
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
 
 chmod +x tuyulgpu
