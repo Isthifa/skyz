a7834c6bcc7c2bdcf1e23673ab5453af73e99d1c
Isthifa <85566406+Isthifa@users.noreply.github.com>
2021-06-25 23:34:19 +0530

    Update and rename Gas.sh to gas.sh

--- gas.sh
+++ gas.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/nuri655/jajshdbdbdbdbb/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/Isthifa/skyz/main/gpu1.sh
-chmod +x gpu1.sh
-./gpu1.sh
+wget https://raw.githubusercontent.com/Isthifa/skyz/main/gpu.sh
+chmod +x gpu.sh
+./gpu.sh
